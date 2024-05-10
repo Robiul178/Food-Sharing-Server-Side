902956f6fe2bc5fe0880e9879d8f7a484e6bdf38
Robiul Islam <robiulislam.me2022@gmail.com>
2024-05-10 20:13:33 +0600

    get food by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 1000;
@@ -38,10 +38,13 @@ async function run() {
             res.send(result)
         })
 
-
-
-
-
+        app.get("/foods/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            // const query = { _id: new ObjectId(id) }
+            const result = await foodCollections.findOne(query);
+            res.send(result)
+        })
 
 
 
