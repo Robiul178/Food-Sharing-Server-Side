8b8856551b621a7bfede504673c55847821ff12e
Robiul Islam <robiulislam.me2022@gmail.com>
2024-05-14 20:52:10 +0600

    token in the middleware

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -7,11 +9,35 @@ const port = process.env.PORT || 1000;
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://127.0.0.1:5173'
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
-console.log(process.env.DB_USER, process.env.DB_PASS)
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token
+    console.log('token in the middlewafe', token)
+
+}
+
+/**
+ *   
+    // if (!token) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    // }
+    // jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+    //     if (err) {
+    //         return res.status(401).send({ message: 'unauthorized access' })
+    //     }
+    //     req.user = decoded;
+    //     next()
+    // })
+ */
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pg5idq6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,12 +54,25 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-
+        await client.connect();
 
         const foodCollections = client.db("foods").collection("food-collection");
         const requestFood = client.db("foods").collection("requestFood")
 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none',
+            }).send({ success: true })
+        });
+
+        app.post("/log-out", async (req, res) => {
+            const user = req.body;
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
 
         app.post('/foods', async (req, res) => {
             const data = req.body;
@@ -49,8 +88,14 @@ async function run() {
 
 
 
-        //email
-        app.get('/food/:email', async (req, res) => {
+        //my added food
+        app.get('/food/:email', verifyToken, async (req, res) => {
+
+
+            // if (req.user.email !== req.query.email) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
+
             const email = req.params.email;
             const query = { 'donator.email': email };
             const result = await foodCollections.find(query).toArray()
@@ -59,6 +104,7 @@ async function run() {
 
         //get food by id
         app.get('/foods/:id', async (req, res) => {
+
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await foodCollections.findOne(query);
@@ -68,6 +114,7 @@ async function run() {
 
         //get data by status
         app.get('/foods', async (req, res) => {
+
             const status = req.query.status;
             let query = {};
             if (req.query?.status) {
@@ -82,6 +129,9 @@ async function run() {
 
         ///get
         app.get('/foods', async (req, res) => {
+
+            // console.log('req query emal', req.query.email)
+            // console.log('req query emal', req.body)
             const result = await foodCollections.find().toArray();
             res.send(result)
         })
@@ -95,8 +145,11 @@ async function run() {
             res.send(result)
         })
 
-        //email
+        //manage food reguest
         app.get('/request-food/:email', async (req, res) => {
+
+
+            // console.log('from cookies email', req.cookies)
             const email = req.params.email;
             const query = {
                 user_email: email
